82057a3d43689277bbcc19d894e61dfbbb04aac9
André Rogério Bononi <61595447+arbononi@users.noreply.github.com>
2025-04-05 16:43:42 -0300

    Add files via upload
    Calculadora.

--- ProjetoCalc.py
+++ ProjetoCalc.py
@@ -2,23 +2,40 @@ from os import system as limp
 
 limp("cls")
 
-#valor1 = 50
-#valor2 = 10
-soma = sub = mult = div = exp = None
-#soma, sub, mult, div, exp = None, None, None, None, None
+opcao = 0
 
-valor1 = float(input("Digite o primeiro valor: "))
-valor2 = float(input("Digite o segundo valor: "))
+while(opcao != 6):
+    limp("cls")
+    
+    soma = sub = mult = div = exp = None
+    #soma, sub, mult, div, exp = None, None, None, None, None
 
-soma = valor1 + valor2
-sub = valor1 - valor1
-mult = valor1 * valor2
-div = valor1 / valor2
-exp = valor1 ** valor2
+    opcao = int(input("Escolha a operação:\n1- Soma\n2- Subtração\n3-Multiplicação\n4- Divisão\n5- Exponenciação\n6-Encerrar\n\n"))
+    
+    if (opcao < 1 or opcao > 6):
+        print("Opção inválida!")
+        continue
+    if (opcao == 6):
+        break
+    
+    valor1 = float(input("Digite o primeiro valor: "))
+    valor2 = float(input("Digite o segundo valor: "))
 
-print(f"O resultado das operações desses valores são:")
-print(f"O resultado da soma: {valor1} + {valor2} = {soma}")
-print(f"O resultado da subtração: {valor1} - {valor2} = {sub}")
-print(f"O resultado da multiplicação: {valor1} * {valor2} = {mult}")
-print(f"O resultado da divisão: {valor1} / {valor2} = {div}")
-print(f"O resultado da exponenciação (potência): {valor1} ** {valor2} = {exp}")
+    soma = valor1 + valor2
+    sub = valor1 - valor1
+    mult = valor1 * valor2
+    div = valor1 / valor2
+    exp = valor1 ** valor2
+
+    if (opcao == 1):
+        print(f"O resultado da soma: {valor1} + {valor2} = {soma}")
+    elif (opcao == 2):
+        print(f"O resultado da subtração: {valor1} - {valor2} = {sub}")
+    elif (opcao == 3):
+        print(f"O resultado da multiplicação: {valor1} * {valor2} = {mult}")
+    elif (opcao == 4):
+        print(f"O resultado da divisão: {valor1} / {valor2} = {div}")
+    elif (opcao == 5):
+        print(f"O resultado da exponenciação (potência): {valor1} ** {valor2} = {exp}")
+    
+    input("Pressione qualquer tecla para continuar...")
